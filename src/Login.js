5804e5cafaf38c763b3172d9c4f27ab210ebd091
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-05-30 18:28:21 +0530

    done

--- src/Login.js
+++ src/Login.js
@@ -23,14 +23,14 @@ function Login() {
 						</div> */}
 						
 						<div className="panel-body">
-							<form name="form" id="form" className="form-horizontal" encType="multipart/form-data" method="POST">
+							<form name="form" id="form" className="form-horizontal" >
 								<div className="input-group">
-									<input id="user" type="text" className="form-control" name="user" placeholder="User" />
+									<input id="user" type="email" className="form-control" name="user" placeholder="User"  required/>
 									<span className="input-group-addon"><i className="glyphicon glyphicon-user" /></span>
 
 								</div>
 								<div className="input-group">
-									<input id="password" type="password" className="form-control" name="password" placeholder="Password" />
+									<input id="password" type="password" className="form-control" name="password" placeholder="Password" required />
 									<span className="input-group-addon"><i className="glyphicon glyphicon-lock" /></span>
 
 								</div>
