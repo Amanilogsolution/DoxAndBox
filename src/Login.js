2ac806a90009c51b2d4c8645c3f1e345620bbab3
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-05-31 11:03:31 +0530

    done

--- src/Login.js
+++ src/Login.js
@@ -7,25 +7,25 @@ function Login() {
 			<div className="container">
 
 				<div id="loginbox" className="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3" >
-			
+
 					<div className="row">
 						<div className="iconmelon" >
-							
+
 							<img src={Image} alt="logo" />
 						</div>
 					</div>
 					<div className="panel-heading">
-							<div className="panel-title text-center text-white " style={{fontSize:"20px",fontWeight:"600"}} >Login</div>
-						</div>
+						<div className="panel-title text-center text-white " style={{ fontSize: "20px", fontWeight: "600" }} >Login</div>
+					</div>
 					<div className="panel panel-default">
 						{/* <div className="panel-heading">
 							<div className="panel-title text-center">Login</div>
 						</div> */}
-						
+
 						<div className="panel-body">
 							<form name="form" id="form" className="form-horizontal" >
 								<div className="input-group">
-									<input id="user" type="email" className="form-control" name="user" placeholder="User"  required/>
+									<input id="user" type="email" className="form-control" name="user" placeholder="User" required />
 									<span className="input-group-addon"><i className="glyphicon glyphicon-user" /></span>
 
 								</div>
@@ -35,24 +35,19 @@ function Login() {
 
 								</div>
 								<div className="form-group">
-									{/* Button */}
-								
-										<button type="submit" href="#" className="btn btn-primary pull-center"> Log in</button>
-								
+									<button type="submit" href="#" className="btn btn-primary pull-center"> Log in</button>
 								</div>
 							</form>
 						</div>
-						
+
 					</div>
 					<div className="bottomdiv">
-                                <p> <a href='#'>forget password ?</a></p>
-                                <p><a href="#">Register</a></p>
-						</div>
+						<p> <a href='#'>forget password ?</a></p>
+						<p><a href="#">Register</a></p>
+					</div>
 				</div>
 			</div>
 			<div>
-
-				
 			</div>
 
 		</>
