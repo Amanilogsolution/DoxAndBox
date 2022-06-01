534ad1239107201132555fece52c077f62c849d5
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-06-01 15:46:31 +0530

    done

--- src/component/Navbar/Sidebar.js
+++ src/component/Navbar/Sidebar.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css'
 
 
 const SideBar = ({ openClass }) => {
+  const [recorddiv,setRecorddiv] =useState(false);
+  const [reportdiv,setReportdiv] =useState(false);
+
+  const handlerecord = ()=>{
+    setRecorddiv(!recorddiv);
+  }
+
+  const handlereport = ()=>{
+    setReportdiv(!reportdiv);
+  }
   return (
     <nav className={openClass === true ? 'closeslidernav slidernav' : ' slidernav'}>
       <ul className="navlist">
@@ -23,33 +33,39 @@ const SideBar = ({ openClass }) => {
           <a className="menu-item" >
 
             <i class="material-icons">arrow_forward_ios</i>
-            Request
-            <ul className="innerul">
+            <span onClick={handlerecord}>Request</span>
+
+           
+          </a>
+
+        </li>
+        {recorddiv ?
+            <ul className="innerul" id='reportinnerdiv'  >
               <li>Record Pickup</li>
               <li>Record Retrieval</li>
               <li>Scanning Request</li>
               <li>Shredding</li>
-              <li>Other Shredding</li>
+              {/* <li>Other Shredding</li> */}
               <li>Other</li>
             </ul>
-          </a>
-
-        </li>
+            :null}
 
         <li>
-        <a className="menu-item" >
+          <a className="menu-item" >
 
-        <i class="material-icons">arrow_forward_ios</i>
-          Reports
-          <ul className="innerul">
-            <li>Inward Report</li>
-            <li>Retrieval Report</li>
-            <li>Scanning Report</li>
-            <li>Stock Report</li>
-
-          </ul>
+          <span onClick={handlereport}><i class="material-icons">arrow_forward_ios</i>
+            Reports</span>
+          
           </a>
         </li>
+        {reportdiv ?
+            <ul className="innerul" id='reportinnerdiv' >
+              <li>Inward Report</li>
+              <li>Retrieval Report</li>
+              <li>Scanning Report</li>
+              <li>Stock Report</li>
+            </ul>
+            :null}
       </ul>
     </nav>
   );
